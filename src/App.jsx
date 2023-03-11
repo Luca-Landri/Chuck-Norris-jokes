9ce420049bf4ed3fb9dfa7a321376667f82abbd8
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-11 11:25:27 +0100

    Feat: Now storybook works

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import styled from 'styled-components'
 import './App.css'
-import Theme from "./assets/Theme";
-
-
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.dark};
-  font-size: ${({ theme }) => theme.fontSizes.h1};
-  font-family: "Dobro-Drunk";
-`
-
-const Container = styled.div`
-  background-color: ${({ theme }) => theme.colors.background};
-  height: 100vh;
-  display: flex;
-`
 
 function App() {
 
   return (
-    <Theme>
-      <Container>
-        <Title>Chuck Norris</Title>
-      </Container>
-    </Theme>
+      <div>
+        <h1>Chuck Norris</h1>
+      </div>
   )
 }
 
