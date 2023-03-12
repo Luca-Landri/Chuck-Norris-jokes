8a5efd86952db964012b45b59dfacb4b14dc4081
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-12 23:35:28 +0100

    Feat: Started creating the first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import "./index.scss"
+import Button from './components/Button'
 
 function App() {
 
   return (
       <div className='container'>
+        <Button text="Click to joke"/>
         <h1>Chuck Norris</h1>
       </div>
   )
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({text, color}) => {
+  return (
+    <div>
+        <h3>{text}</h3>
+    </div>
+  )
+}
+
+export default Button
