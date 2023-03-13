bc7e6afd45891d83d91b6bbc32753e2cad27dbeb
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-13 08:37:24 +0100

    Feat: added various style to the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 import "./index.scss"
 import Button from './components/Button/Button'
+import React, { useEffect } from 'react'
 
 function App() {
 
   return (
       <div className='container'>
-        <h1>Chuck Norris</h1>
+        <h1 className='title'>Joke Norris</h1>
+        <Button text="Copy to clipboard 📔" type="copy"/>
         <Button text="Click to joke" type="primary"/>
       </div>
   )
