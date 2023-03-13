9a7eea2c21b7968b186bdb96b7b28ddc0f90522e
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-13 11:27:25 +0100

    Fix: App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./index.scss"
 import Button from './components/button/Button'
 import React, { useEffect, useState } from 'react'
 import Category from './components/body/Category'
+import Joke from './components/body/Joke'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('')
@@ -14,7 +15,10 @@ function App() {
   return (
       <div className='container'>
         <h1 className='title'>Joke Norris</h1>
-        <Category take={setSelectedCategory} text="Select a category"/>
+        <div className='body'>
+          <Category take={setSelectedCategory} text="Select a category"/>
+          <Joke joke="Chuck Norris doesn't need a debugger, he just stares down the bug until the code confesses."/>
+        </div>
         <div className='buttons'>
           <Button text="Copy to clipboard 📔" type="copy"/>
           <Button text="Click to joke" type="primary"/>
