227864950910144e1b46032997456a398ccec97e
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-13 09:17:33 +0100

    Feat: Now the front-end navbar works

--- src/components/body/Category.jsx
+++ src/components/body/Category.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Category.scss'
 import PropTypes from 'prop-types'
 import { Icon } from '@iconify/react';
 
+
 const Category = () => {
-  return (
-    <div className='category-container'>
-        <h4>Select a category</h4>
-        <Icon icon="material-symbols:arrow-back-ios-new-rounded" width="30" height="30" rotate={3} />
-    </div>
-  )
+    const [category, setCategory] = useState(false)
+
+    return (
+        <div onClick={() => setCategory(!category)} className='category-container'>
+            <div className='selection'>
+                <h4>Select a category</h4>
+                <Icon icon="material-symbols:arrow-back-ios-new-rounded" width="30" height="30" rotate={3} />
+            </div>
+            {
+                category && (
+                    <div className='category'>
+                        <p>animal</p>
+                        <p>career</p>
+                        <p>celebrity</p>
+                        <p>dev</p>
+                    </div>
+            )}                
+        </div>
+    )
 }
 
 export default Category
