86e099ef2c57f8b5da2fd999df4f8429dca93ad3
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-13 09:35:11 +0100

    Feat: Now scroll works on category selection

--- src/components/body/Category.jsx
+++ src/components/body/Category.jsx
@@ -1,25 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Category.scss'
 import PropTypes from 'prop-types'
 import { Icon } from '@iconify/react';
 
 
+
 const Category = () => {
-    const [category, setCategory] = useState(false)
+    const [open, setOpen] = useState(false)
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+    fetch(`https://api.chucknorris.io/jokes/categories`)
+    .then((response) => response.json())
+    .then((actualData) => {
+        console.log(actualData)
+        setCategories(actualData)
+    })
+    .catch((err) => console.log(err.message));
+
+    console.log(categories)
+    }, []);
 
     return (
-        <div onClick={() => setCategory(!category)} className='category-container'>
+        <div onClick={() => setOpen(!open)} className='category-container'>
             <div className='selection'>
                 <h4>Select a category</h4>
                 <Icon icon="material-symbols:arrow-back-ios-new-rounded" width="30" height="30" rotate={3} />
             </div>
             {
-                category && (
+                open && (
                     <div className='category'>
-                        <p>animal</p>
-                        <p>career</p>
-                        <p>celebrity</p>
-                        <p>dev</p>
+                        {categories.map((category) => (
+                                <p>{category}</p>
+                        ))}
                     </div>
             )}                
         </div>
