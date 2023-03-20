cb2f8cd77e6b3f12502501492d5559263ad9734d
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-03-20 12:46:43 +0100

    Feat: Copy to clipboard works

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './App.scss'
 import "./index.scss"
 import Button from './components/button/Button'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Category from './components/body/Category'
 import Joke from './components/body/Joke'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('')
   const [joke, setJoke] = useState('')
-  const [copy, setCopy] = useState('')
   const [error, setError] = useState(false)
 
   const generateJoke = () => {
     fetch(`https://api.chucknorris.io/jokes/random?category=${selectedCategory.category}`)
       .then((response) => response.json())
       .then((actualData) => {
-        console.log(actualData.value)
         setJoke(actualData.value)
       })
       .catch((err) => console.log(err.message));
   }
+
+  const copyToClipboard = (e) => {
+    navigator.clipboard.writeText(e.joke)
+  }
+
 
   return (
       <div className='container'>
@@ -32,7 +35,7 @@ function App() {
           <Joke joke={joke}/>
         </div>
         <div className='buttons'>
-          <Button text="Copy to clipboard 📔" type="copy"/>
+          <Button text="Copy to clipboard 📔" type="copy" action={copyToClipboard({joke})}/>
           <Button text="Click to joke" type="primary" action={() => generateJoke()}
           />
         </div>
